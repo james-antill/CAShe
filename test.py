d8298ba22f0d2a851fbb83c5a56a9ec4be768dfa
James Antill <james@and.org>
2015-03-05 02:23:45 -0500

    Add first cleanup tests.

--- test.py
+++ test.py
@@ -16,8 +16,23 @@ def _cleanup(tdir):
 # Data to sha256 checksums ...
 d2s = {
     'a'     :'ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb',
-    'aa'    :'961b6dd3ede3cb8ecbaacbd68de040cd78eb2ed5889130cceb4c49268ea4d506',
+    'a'*2   :'961b6dd3ede3cb8ecbaacbd68de040cd78eb2ed5889130cceb4c49268ea4d506',
+    'a'*3   :'9834876dcfb05cb167a5c24953eba58c4ac89b1adf57f28f2f9d09af107ee8f0',
+    'a'*4   :'61be55a8e2f6b4e172338bddf184d6dbee29c98853e0a0485ecee7f27b9af0b4',
+    'a'*5   :'ed968e840d10d2d313a870bc131a4e2c311d7ad09bdf32b3418147221f51a6e2',
+    'a'*6   :'ed02457b5c41d964dbd2f2a609d63fe1bb7528dbe55e1abf5b52c249cd735797',
+    'a'*7   :'e46240714b5db3a23eee60479a623efba4d633d27fe4f03c904b9e219a7fbe60',
+    'a'*8   :'1f3ce40415a2081fa3eee75fc39fff8e56c22270d1a978a7249b592dcebd20b4',
+    'a'*9   :'f2aca93b80cae681221f0445fa4e2cae8a1f9f8fa1e1741d9639caad222f537d',
+
     'b'     :'3e23e8160039594a33894f6564e1b1348bbd7a0088d42c4acb73eeaed59c009d',
+    'b'*2   :'3b64db95cb55c763391c707108489ae18b4112d783300de38e033b4c98c3deaf',
+    'b'*3   :'3e744b9dc39389baf0c5a0660589b8402f3dbb49b89b3e75f2c9355852a3c677',
+    'b'*4   :'81cc5b17018674b401b42f35ba07bb79e211239c23bffe658da1577e3e646877',
+    'b'*5   :'5e846c64f2db12266e6b658a8e5b5b42cc225419b3ee1fca88acbb181ddfdb52',
+    'b'*6   :'4625fd63b0e96fc0d656ae7381605e48d4a0f63a319fc743adf22688613883c7',
+    'b'*7   :'ea415a61bd19915084366a0a2fdaebe070a9c3168877ecdb5e36f4905b5f8aa3',
+    'b'*8   :'fb398cc690e15ddba43ee811b6c0d3ec190901ad3df377fec9a1f9004b919a06',
     }
 
 # End of setup
@@ -149,7 +164,6 @@ class Cashe_tests(unittest.TestCase):
         self.assertPathLinks(co.filename, 3)
         self.assertTrue(co.exists)
         self.assertEqual(1, len(list(x.ls())))
-        # print "JDBG: ls:", list(x.ls())
 
     def test3_save_3(self):
         x = cashe.CAShe(self.tdir + "/test3")
@@ -238,6 +252,50 @@ class Cashe_tests(unittest.TestCase):
         self.assertTrue(co.exists)
 
         self.assertEqual(len(co), 2)
+
+    def test3_1_cleanup(self):
+        x = cashe.CAShe(self.tdir + "/test3")
+
+        def _put(d):
+            datai = x.path + "/data3.1" + d
+            fwrite(datai, d)
+
+            co = x.get('sha256', d2s[d])
+            co.save(datai, link=False)
+        for num in range(1, 9):
+            _put("a"*num)
+            _put("b"*num)
+        fwrite(x.path + "/config", "lo = 40 \n hi = 40\n")
+        self.assertEqual(16, len(list(x.ls())))
+        self.assertEqual((40, 40, 8 * 60 * 60 * 24, "atime"), x._get_config())
+        x.cleanup()
+        self.assertEqual(5, len(list(x.ls())))
+        fwrite(x.path + "/config", "lo = 4 \n hi = 4\n")
+        self.assertEqual((4, 4, 8 * 60 * 60 * 24, "atime"), x._get_config())
+        x.cleanup()
+        self.assertEqual(0, len(list(x.ls())))
+
+    def test3_2_cleanup(self):
+        x = cashe.CAShe(self.tdir + "/test3")
+
+        def _put(d):
+            datai = x.path + "/data3.2" + d
+            fwrite(datai, d)
+
+            co = x.get('sha256', d2s[d])
+            co.save(datai, link=False)
+        for num in reversed(range(1, 9)):
+            _put("a"*num)
+            _put("b"*num)
+        fwrite(x.path + "/config", "lo = 40 \n hi = 40\n")
+        self.assertEqual(16, len(list(x.ls())))
+        self.assertEqual((40, 40, 8 * 60 * 60 * 24, "atime"), x._get_config())
+        x.cleanup()
+        self.assertEqual(11, len(list(x.ls())))
+        fwrite(x.path + "/config", "lo = 4 \n hi = 4\n")
+        self.assertEqual((4, 4, 8 * 60 * 60 * 24, "atime"), x._get_config())
+        x.cleanup()
+        self.assertEqual(3, len(list(x.ls())))
 
 
 
